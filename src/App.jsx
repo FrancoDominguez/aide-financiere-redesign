314174b3aae3baf5244f12c2376bb3faf1cb2266
Jonathan <jonathandominicpasquin@gmail.com>
2025-04-10 17:40:23 -0400

    Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Application from './Application.page';
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="container mx-auto p-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">Aide Financière Redesign</h1>
-        <p className="text-gray-700">Welcome to the clean project.</p>
       </div>
+      <Application></Application>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
     </div>
   )
 }
---
+++ src/Application.page.jsx
@@ -0,0 +1,9 @@
+function Application() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      Application
+    </div>
+  );
+}
+
+export default Application;
