ee95a3b61dea3c50e9824cfcabf0a500d16020fb
Jonathan <jonathandominicpasquin@gmail.com>
2025-04-11 01:19:33 -0400

    Application page

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import LoginPage from "./pages/loginPage";
 import MyAccountPage from "./pages/myAccountPage";
 import SignupPage from "./pages/signupPage";
 import LandingPage from "./pages/landingPage";
+import ApplicationPage from "./pages/applicationPage"
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
+    <div className="min-h-screen flex flex-col bg-white">
       <Header />
-      <main className="flex-grow container mx-auto my-4 p-6 bg-white rounded-lg shadow-md flex items-center justify-center">
+      <main className="flex-grow container mx-auto p-6 block items-center justify-center">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/my-account" element={<MyAccountPage />} />
+          <Route path="/application" element={<ApplicationPage />} />
         </Routes>
       </main>
       <Footer />
--- src/pages/applicationPage.jsx
+++ src/pages/applicationPage.jsx
@@ -1,7 +1,71 @@
+import TextField from "@mui/material/TextField";
+
 export default function ApplicationPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      Application
+    <div>
+      <div className="text-center text-3xl text-[#263652] font-bold">
+        <h1>Loans and Bursaries</h1>
+        <h1>Full-Time Studies Application</h1>
+      </div>
+
+      <div className="w-full m-auto max-w-[400px]">
+        <h1 className="mt-4 text-lg text-[#263652] font-bold">Personal Information</h1>
+        <TextField
+          label="Full Name"
+          size="small"
+          fullWidth
+          sx={{
+            mt: 1.5,
+            '& .MuiOutlinedInput-root': {
+              borderRadius: '8px',
+            }
+          }}
+        />
+        <TextField
+          label="Social Insurance Number"
+          size="small"
+          fullWidth
+          sx={{
+            mt: 1.5,
+            '& .MuiOutlinedInput-root': {
+              borderRadius: '8px',
+            }
+          }}
+        />
+        <TextField
+          label="Address"
+          size="small"
+          fullWidth
+          sx={{
+            mt: 1.5,
+            '& .MuiOutlinedInput-root': {
+              borderRadius: '8px',
+            }
+          }}
+        />
+        <TextField
+          label="Telephone Number"
+          size="small"
+          fullWidth
+          sx={{
+            mt: 1.5,
+            '& .MuiOutlinedInput-root': {
+              borderRadius: '8px',
+            }
+          }}
+        />
+        <TextField
+          label="Date of Permanent Residence Status"
+          size="small"
+          fullWidth
+          sx={{
+            mt: 1.5,
+            '& .MuiOutlinedInput-root': {
+              borderRadius: '8px',
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
